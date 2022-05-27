13f7526162f3f9cbb7da44a8e37e4cb182068124
Sucareto <28331534+Sucareto@users.noreply.github.com>
2022-05-27 23:08:01 +0800

    重写，修正Bug。
    参考：https://github.com/xiaopeng12138/MaiDXR/blob/main/Assets/Scripts/LedSerial.cs

--- Mai2LED/Mai2LED.ino
+++ Mai2LED/Mai2LED.ino
@@ -1,12 +1,12 @@
 #if defined(__AVR_ATmega32U4__) || defined(ARDUINO_SAMD_ZERO)
 #pragma message "当前的开发板是 ATmega32U4 或 SAMD_ZERO"
 #define SerialDevice SerialUSB
-#define DATA_PIN A0
+#define LED_PIN 13
 
 #elif defined(ARDUINO_ESP8266_NODEMCU_ESP12E)
 #pragma message "当前的开发板是 NODEMCU_ESP12E"
 #define SerialDevice Serial
-#define DATA_PIN D5
+#define LED_PIN D5
 
 #elif defined(ARDUINO_NodeMCU_32S)
 #pragma message "当前的开发板是 NodeMCU_32S"
@@ -19,16 +19,17 @@
 #endif
 
 #include "FastLED.h"
-#define NUM_LEDS 60
+#define NUM_LEDS 11
 CRGBArray<NUM_LEDS> leds;
 
+
 enum {
-  SetLedDirectCommand = 0x82,
-  SetLedFetCommand = 0x39,
-  SetLedGs8BitCommand = 0x31,//49
-  SetLedGs8BitMultiCommand = 0x32,//50
-  SetLedGs8BitMultiFadeCommand = 0x33,//51
-  SetLedGsUpdateCommand = 0x3C,
+  LedGs8Bit = 0x31,//49
+  LedGs8BitMulti = 0x32,//50
+  LedGs8BitMultiFade = 0x33,//51
+  LedFet = 0x39,//57
+  LedGsUpdate = 0x3C,//60
+  LedDirect = 0x82,
 };
 
 typedef union {
@@ -49,15 +50,15 @@ typedef union {
         uint8_t ExtLed;
         uint8_t SideLed;
       };
-      struct { //10
-        uint8_t index;//<11
+      struct { //LedGs8Bit
+        uint8_t index;
         uint8_t r;
         uint8_t g;
         uint8_t b;
       };
-      struct { //13
+      struct { //LedGs8BitMulti,LedGs8BitMultiFade
         uint8_t start;
-        uint8_t end;
+        uint8_t end;//length
         uint8_t skip;
         uint8_t mr;
         uint8_t mg;
@@ -104,47 +105,51 @@ void setup() {
   FastLED.setBrightness(30);
   FastLED.clear();
   SerialDevice.begin(115200);
-  leds(0, 8) = 0xFF00FF;
+  leds(0, 7) = 0xFFFFFF;
   FastLED.delay(1000);
 }
-int progress = 0;
-uint8_t fade_ms, fade_start, fade_end;
-uint32_t last_ms;
+
+unsigned long fade_start, fade_end, progress;
+uint8_t led_start, led_end;
 CRGB fade_prev, fade_taget;
 
 void loop() {
   switch (packet_read()) {
-    case SetLedDirectCommand:
+    case LedGs8Bit:
+      leds[req.index] = CRGB(req.r, req.g, req.b);
       break;
-    case SetLedFetCommand://框体灯，只有白色，值代表亮度，会多次发送实现渐变，需要立刻刷新
-      //      leds(36, 38) = blend(0x000000, 0xFFFFFF, req.BodyLed);
-      //      leds(39, 41) = blend(0x000000, 0xFFFFFF, req.ExtLed);
-      //      leds(42, 44) = blend(0x000000, 0xFFFFFF, req.SideLed);
-      //      FastLED.show();
+
+    case LedGs8BitMulti:
+      if (req.end == 0x20) {
+        req.end = NUM_LEDS;
+      }
+      leds(req.start, req.end - 1) = CRGB(req.mr, req.mg, req.mb);
+      fade_prev = CRGB(req.mr, req.mg, req.mb);
       break;
-    case SetLedGs8BitCommand://设置 req.index 号键的灯为 CRGB(req.r, req.g, req.b)
-      leds(req.index * 6, (req.index + 1) * 6) = CRGB(req.r, req.g, req.b);
-      break;
-    case SetLedGs8BitMultiCommand://设置 req.start 到 req.end 的灯为 CRGB(req.mr, req.mg, req.mb)
-      leds(req.start * 6, req.end * 6) = CRGB(req.mr, req.mg, req.mb);
-      break;
-    case SetLedGs8BitMultiFadeCommand://非正确实现的渐变，具体情况参考 Mai2LED-Data.txt
-      fade_prev = leds(req.start, req.end);
+
+    case LedGs8BitMultiFade:
       fade_taget = CRGB(req.mr, req.mg, req.mb);
-      progress = fade_ms = 4096 / req.speed * 8;
-      fade_start = req.start;
-      fade_end = req.end;
-      last_ms = millis();
+      fade_start = millis();
+      fade_end = fade_start + (4095 / req.speed * 8);
+      led_start = req.start;
+      led_end = req.end - 1;
+      leds(led_start, led_end) = CRGB(req.mr, req.mg, req.mb);
       break;
-    case SetLedGsUpdateCommand://提交灯光数据
+
+    case LedFet://框体灯，只有白色，值代表亮度，会多次发送实现渐变，需要立刻刷新
+      leds[8] = blend(0x000000, 0xFFFFFF, req.BodyLed);
+      leds[9] = blend(0x000000, 0xFFFFFF, req.ExtLed);//same as BodyLed
+      leds[10] = blend(0x000000, 0xFFFFFF, req.SideLed);//00 or FF
       FastLED.show();
       break;
-    case 0:
+
+    case LedGsUpdate://提交灯光数据
+      FastLED.show();
       break;
   }
-  if (!progress)return;//非正确实现的渐变
-  progress = fade_ms - (millis() - last_ms);
-  if (progress <= 0)progress = 0;
-  leds(fade_start * 4, fade_end * 4 + 4) = blend(fade_taget, fade_prev, map(progress, 0, fade_ms, 0, 255));
+
+  if (millis() > fade_end)return;
+  progress = map(millis(), fade_start, fade_end, 0, 255);
+  leds(led_start, led_end) = blend(fade_taget, fade_prev, progress);
   FastLED.show();
 }
